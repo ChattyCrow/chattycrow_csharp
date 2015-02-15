59562656a8e3d63daecfdc6432222aecc843c4e5
Strnadj <jan.strnadek@gmail.com>
2015-02-15 09:38:25 +0100

    Initiate

---
+++ ChattyCrow.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChattyCrow
+{
+    public class ChattyCrow
+    {
+        /// <summary>
+        /// Default host end point.
+        /// </summary>
+        public const String DEFAULT_HOST = "https://chattycrow.com/api/v1/";
+
+        /// <summary>
+        /// Create basic chattycrow class.
+        /// </summary>
+        /// <param name="DefaultToken">Default token</param>
+        /// <param name="DefaultChannel">Default channel</param>
+        /// <param name="host">Default host (optional)</param>
+        public ChattyCrow(String DefaultToken, String DefaultChannel, String host = DEFAULT_HOST)
+        {
+ 
+        }
+    }
+}

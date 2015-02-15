e9cb311fa0dc569818a65699a59a17857a4d0f2e
Strnadj <jan.strnadek@gmail.com>
2015-02-15 22:45:12 +0100

    Finall appveyor + coveralls!

--- ChattyCrow.Tests/Properties/Resources.Designer.cs
+++ ChattyCrow.Tests/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace chattycrowtest.Properties {
+namespace ChattyCrow.Tests.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace chattycrowtest.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("chattycrowtest.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ChattyCrow.Tests.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
